29c3e1e206932e7dc743f5974f066c11d261085d
Shane Ratledge <honorthrawn@gmail.com>
2023-03-13 00:12:32 -0400

    Add count of turns left to planet scene when terraforming in progress Fix some warnings

--- src/commonMain/kotlin/PlanetsScene.kt
+++ src/commonMain/kotlin/PlanetsScene.kt
@@ -11,6 +11,7 @@ class PlanetsScene(val gs: GalaxyState, val es: EmpireState, val ps: PlayerState
 
     private val direction = mutableListOf<Boolean>()
     private lateinit var notEnoughDialog: RoundRect
+    private var planetTexts = mutableListOf<Text>()
 
     override suspend fun SContainer.sceneInit() {
         val font = resourcesVfs["fonts/bioliquid-Regular.ttf"].readTtfFont()
@@ -19,15 +20,14 @@ class PlanetsScene(val gs: GalaxyState, val es: EmpireState, val ps: PlayerState
         val startx = 200
         var starty = 600
 
-        val background = image(resourcesVfs["ui/hs-2012-37-a-large_web.jpg"].readBitmap())
+        image(resourcesVfs["ui/hs-2012-37-a-large_web.jpg"].readBitmap())
         {
             position(0, 0)
             setSizeScaled(width, height)
         }
 
-        for((i, planet) in gs.stars[ps.activePlayerStar]!!.planets.values.withIndex())
-        {
-            val fileName = when( planet.type) {
+        for ((i, planet) in gs.stars[ps.activePlayerStar]!!.planets.values.withIndex()) {
+            val fileName = when (planet.type) {
                 PlanetType.TOXIC -> "planets/planet1.png"
                 PlanetType.OCEAN -> "planets/planet2.png"
                 PlanetType.TERRAN -> "planets/planet3.png"
@@ -42,42 +42,40 @@ class PlanetsScene(val gs: GalaxyState, val es: EmpireState, val ps: PlayerState
                 position(startx, starty)
             }
             direction.add(i, false)
-            planetImage.addUpdater { updatePlanet(planetImage,i) }
-            planetImage.onClick { planetClicked(i, font)  }
 
-            val planetTextColor = when(planet.ownerIndex)
-            {
+            val planetTextColor = when (planet.ownerIndex) {
                 Allegiance.Unoccupied -> Colors.WHITE
                 Allegiance.Player -> Colors.CYAN
                 Allegiance.Enemy -> Colors.RED
             }
 
-            var turnCounter = gs.stars[ps.activePlayerStar]!!.planets[index]!!.turnsLeftTerraform
-            val planetTxt: String
-            if(turnCounter == -1) {
-                planetTxt = "${planet.name} - ${planet.type}"
-            } else
-            {
-                planetTxt = "${planet.name} - ${planet.type} ($turnCounter)"
+            val turnCounter = gs.stars[ps.activePlayerStar]!!.planets[index]!!.turnsLeftTerraform
+            val planetTxt: String = if (turnCounter == -1) {
+                "${planet.name} - ${planet.type}"
+            } else {
+                "${planet.name} - ${planet.type} $turnCounter"
             }
-            text( planetTxt, 50.00, planetTextColor, font)
+            planetTexts.add(i, text(planetTxt, 50.00, planetTextColor, font)
             {
                 centerXOn(planetImage)
                 alignTopToTopOf(planetImage, 12.0)
             }
+            )
+            planetImage.addUpdater { updatePlanet(planetImage, i) }
+            planetImage.onClick { planetClicked(i) }
             starty -= 200
-          }
+        }
 
-        val fileName = when( gs.stars[ps.activePlayerStar]!!.type) {
+        val fileName = when (gs.stars[ps.activePlayerStar]!!.type) {
             StarType.YELLOW -> "stars/Star cK gK eg9.bmp"
             StarType.BLUE -> "stars/Star B supeg5.bmp"
             StarType.RED -> "stars/Star M supeg5.bmp"
         }
         val starImage = image(resourcesVfs[fileName].readBitmap()) {
             scale(0.5)
-            position( width/2, 800.00)
+            position(width / 2, 800.00)
         }
-        text( gs.stars[ps.activePlayerStar]!!.name, 50.00, Colors.CYAN, font)
+        text(gs.stars[ps.activePlayerStar]!!.name, 50.00, Colors.CYAN, font)
         {
             centerXOn(starImage)
             alignTopToTopOf(starImage, 12.0)
@@ -101,40 +99,33 @@ class PlanetsScene(val gs: GalaxyState, val es: EmpireState, val ps: PlayerState
         }
     }
 
-    private suspend fun Container.planetClicked(index: Int, font: Font)
-    {
-        when(ps.operation) {
-            operationType.SELECTION -> selectPlanet(index, font)
+    private suspend fun planetClicked(index: Int) {
+        when (ps.operation) {
+            operationType.SELECTION -> selectPlanet(index)
             operationType.MOVINGFLEET -> showNoGo("Invalid Mode") //shouldn't happen
-            operationType.COLONIZE -> colonizePlanet(index, font)
-            operationType.TERRAFORM -> terraformPlanet(index, font)
+            operationType.COLONIZE -> colonizePlanet(index)
+            operationType.TERRAFORM -> terraformPlanet(index)
         }
     }
 
-    private suspend fun terraformPlanet(index: Int, font: Font)
-    {
-        val message = "turns left: ${gs.stars[ps.activePlayerStar]!!.planets[index]!!.turnsLeftTerraform}"
-        println(message)
+    private suspend fun terraformPlanet(index: Int) {
+        //val message = "turns left: ${gs.stars[ps.activePlayerStar]!!.planets[index]!!.turnsLeftTerraform}"
+        //println(message)
         if (gs.stars[ps.activePlayerStar]!!.planets[index]!!.ownerIndex == Allegiance.Unoccupied) {
-            if (gs.stars[ps.activePlayerStar]!!.playerFleet!!.getTerraformersCount() >= 1) {
-                if(gs.stars[ps.activePlayerStar]!!.planets[index]!!.type != PlanetType.SUPERTERRAN) {
+            if (gs.stars[ps.activePlayerStar]!!.playerFleet.getTerraformersCount() >= 1) {
+                if (gs.stars[ps.activePlayerStar]!!.planets[index]!!.type != PlanetType.SUPERTERRAN) {
                     if (gs.stars[ps.activePlayerStar]!!.planets[index]!!.turnsLeftTerraform == -1) {
                         ps.terraformingIndex = index
                         gs.stars[ps.activePlayerStar]!!.planets[ps.terraformingIndex]!!.startTerraforming()
-                        gs.stars[ps.activePlayerStar]!!.playerFleet!!.removeShipFromFleet(shipType.TERRAFORMATTER_HUMAN)
+                        gs.stars[ps.activePlayerStar]!!.playerFleet.removeShipFromFleet(shipType.TERRAFORMATTER_HUMAN)
                         sceneContainer.changeTo<terraformingScene>()
-                    } else
-                    {
+                    } else {
                         showNoGo("This world is already being terraformed!")
                     }
-                }
-                else
-                {
+                } else {
                     showNoGo("This world is as good as it gets!")
                 }
-            }
-            else
-            {
+            } else {
                 showNoGo("You must have at least one Terraformer in system to terraform the world")
             }
         } else {
@@ -143,8 +134,7 @@ class PlanetsScene(val gs: GalaxyState, val es: EmpireState, val ps: PlayerState
         ps.operation = operationType.SELECTION
     }
 
-    private suspend fun selectPlanet(index: Int, font: Font)
-    {
+    private suspend fun selectPlanet(index: Int) {
         if (gs.stars[ps.activePlayerStar]!!.planets[index]!!.ownerIndex == Allegiance.Player) {
             ps.activePlayerPlanet = index; sceneContainer.changeTo<PlanetScene>()
         } else {
@@ -152,58 +142,65 @@ class PlanetsScene(val gs: GalaxyState, val es: EmpireState, val ps: PlayerState
         }
     }
 
-    private suspend fun colonizePlanet(index: Int, font: Font)
-    {
+    private suspend fun colonizePlanet(index: Int) {
         if (gs.stars[ps.activePlayerStar]!!.planets[index]!!.ownerIndex == Allegiance.Unoccupied) {
-            if (gs.stars[ps.activePlayerStar]!!.playerFleet!!.getColonyShipCount() >= 1) {
+            if (gs.stars[ps.activePlayerStar]!!.playerFleet.getColonyShipCount() >= 1) {
                 ps.activePlayerPlanet = index
                 gs.stars[ps.activePlayerStar]!!.planets[index]!!.ownerIndex = Allegiance.Player
                 gs.stars[ps.activePlayerStar]!!.planets[index]!!.farmers = 1u
-                gs.stars[ps.activePlayerStar]!!.playerFleet!!.removeShipFromFleet(shipType.COLONY_HUMAN)
+                gs.stars[ps.activePlayerStar]!!.playerFleet.removeShipFromFleet(shipType.COLONY_HUMAN)
                 ps.operation = operationType.SELECTION
                 sceneContainer.changeTo<ColonyScene>()
             } else {
                 showNoGo("At least one colony ship in system to establish colony")
             }
-        } else
-        {
+        } else {
             showNoGo("Planet must be unoccupied to establish colony")
         }
         ps.operation = operationType.SELECTION
     }
 
 
-    private fun updatePlanet(planet: Image, index: Int)
-    {
-        if(planet.x >= sceneWidth - planet.width) {
+    private fun updatePlanet(planet: Image, index: Int) {
+        if (planet.x >= sceneWidth - planet.width) {
             direction[index] = true
         }
-        if(planet.x <= 0 )
-        {
+        if (planet.x <= 0) {
             direction[index] = false
         }
-        if(direction[index])
-        {
+        if (direction[index]) {
             planet.x -= (index + 1)
-        } else
-        {
+        } else {
             planet.x += (index + 1)
         }
+
+        val planetTextColor = when (gs.stars[ps.activePlayerStar]!!.planets[index]!!.ownerIndex) {
+            Allegiance.Unoccupied -> Colors.WHITE
+            Allegiance.Player -> Colors.CYAN
+            Allegiance.Enemy -> Colors.RED
+        }
+
+        val turnCounter = gs.stars[ps.activePlayerStar]!!.planets[index]!!.turnsLeftTerraform
+        val planetTxt: String = if (turnCounter == -1) {
+            "${gs.stars[ps.activePlayerStar]!!.planets[index]!!.name} - ${gs.stars[ps.activePlayerStar]!!.planets[index]!!.type}"
+        } else {
+            "${gs.stars[ps.activePlayerStar]!!.planets[index]!!.name} - ${gs.stars[ps.activePlayerStar]!!.planets[index]!!.type} $turnCounter"
+        }
+        planetTexts[index].text = planetTxt
+        planetTexts[index].color = planetTextColor
     }
 
     private suspend fun showNoGo(requirements: String) {
         val font = resourcesVfs["fonts/bioliquid-Regular.ttf"].readTtfFont()
         notEnoughDialog =
-            this.sceneContainer.container().roundRect(sceneWidth/2.00, sceneHeight / 4.00, 5.0, 5.0,
-                Colors.BLACK)
+            this.sceneContainer.container().roundRect(
+                sceneWidth / 2.00, sceneHeight / 4.00, 5.0, 5.0,
+                Colors.BLACK
+            )
             {
                 centerOnStage()
                 uiVerticalStack {
-                    width = sceneWidth / 2.00
-                    //text("Not enough resources", 50.00, Colors.CYAN, font)
-                    //{
-                    //    autoScaling = true
-                    //}
+                    scaledWidth = sceneWidth / 2.00
                     text(requirements, 50.00, Colors.CYAN, font)
                     text("CLOSE", 50.00, Colors.GOLD, font)
                     {
@@ -214,8 +211,7 @@ class PlanetsScene(val gs: GalaxyState, val es: EmpireState, val ps: PlayerState
             }
     }
 
-    private fun closeMessage()
-    {
+    private fun closeMessage() {
         notEnoughDialog.removeFromParent()
     }
 }
